6ba82578433351f0ce150fc7ab9d5cd648dd1a77
atakanata <atakanata@gmail.com>
2020-03-04 22:03:42 -0500

    day 4 with last

--- src/test/java/com/automation/tests/day4/FindElementsTask.java
+++ src/test/java/com/automation/tests/day4/FindElementsTask.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindElementsTask {
@@ -14,10 +15,34 @@ public class FindElementsTask {
         driver.get("http://practice.cybertekschool.com/");
         Thread.sleep(3000);
         List<WebElement> links =  driver.findElements(By.tagName("a"));
-        for (WebElement each:links
+        for (WebElement link:links
              ) {
-            System.out.println(each.getText());
+           // System.out.println(each.getText());
+//            each.click();
+//            Thread.sleep(2000);
+//            driver.navigate().back();
+//            links = driver.findElements(By.tagName("a"));
         }
+
+        //List<String> weblinks = new ArrayList<>();
+        for (int i = 1; i <links.size() -1; i++) {
+            links.get(i).click();
+            driver.navigate().back();
+            links = driver.findElements(By.tagName("a"));
+//            weblinks.add(links.get(i).getAttribute("href"));
+//            //System.out.println(links.get(i).getAttribute("href") );
+//            //driver.navigate().to(links.get(i).getAttribute("href"));
+//            //driver.navigate().back();
+////            Thread.sleep(2000);
+////            driver.navigate().back();
+////            Thread.sleep(2000);
+//            //refresh list
+        }
+//        for (String weblink:weblinks
+//             ) {
+//            driver.navigate().to(weblink);
+//            Thread.sleep(2000);
+//        }
 
         driver.quit();
 
---
+++ src/test/java/com/automation/tests/day4/VerfyThatElementIsGone.java
@@ -0,0 +1,35 @@
+package com.automation.tests.day4;
+
+import com.automation.utilities.DriverFactory.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class VerfyThatElementIsGone {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        Thread.sleep(2000);
+        driver.findElement(By.id("disappearing_button")).click();
+        Thread.sleep(2000);
+
+        if (driver.findElements(By.id("disappearing_button")).size()==0){
+            System.out.println("test passed");
+        }else{
+            System.out.println("test failed");
+        }
+
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+        for (WebElement button:buttons
+             ) {
+            button.click();
+            Thread.sleep(1000);
+        }
+
+        driver.quit();
+
+
+    }
+}
